668a972b2d723373100c0eb502fa1d94a149f399
maoyln <maoyl_yx@163.com>
2024-10-29 17:11:01 +0800

    防抖

---
+++ 009防抖与节流/防抖.js
@@ -0,0 +1,20 @@
+function debounce(fn, wait) {
+  var time = null;
+
+  return function() {
+    var context =this;
+    var arg = [...arguments];
+
+    // 如果此时存在定时器的话，则取消之前的定时器重新记时
+    if (time) {
+      clearTimeout(time);
+      time = null;
+    }
+
+    // 设置定时器，使事件间隔指定事件后执行
+    time = setTimeout(() => {
+     fn.apply(context, arguments);
+    }, wait);
+  }
+
+}
