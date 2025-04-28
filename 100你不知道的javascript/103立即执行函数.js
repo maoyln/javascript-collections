dc80711462104acd2b89202231e7b089e2f62d21
maoyln <maoyl_yx@163.com>
2025-04-28 13:38:51 +0800

    作用域

--- 100你不知道的javascript/103立即执行函数.js
+++ 100你不知道的javascript/103立即执行函数.js
@@ -1,8 +1,8 @@
 var a = 2;
 
-(function(global) {
+(function(gl) {
   var a = 3;
 
   console.log(a); // 3（当前作用域的 a）
-  console.log(global.a); // 2（全局作用域的 a）
+  console.log(gl.a); // 2（全局作用域的 a）
 })(typeof window !== 'undefined' ? window : global); // 兼容浏览器和Node.js
