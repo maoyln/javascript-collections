b86c6d836a744b764e1d970f814501c83d827256
maoyln <maoyl_yx@163.com>
2025-04-28 17:00:38 +0800

    闭包的理解-实例

--- 100你不知道的javascript/105闭包的理解.js
+++ 100你不知道的javascript/105闭包的理解.js
@@ -3,5 +3,38 @@ function wait(message){
       console.log(message);
     }, 1000)
 }
-
 wait("Hello, World!");
+
+
+for ( var i = 0; i < 5; i++) {
+    wait(i);
+}
+
+
+for ( var i = 1; i <= 5; i++) {
+  setTimeout(function timer(){
+    console.log(i);
+  }, i * 1000)
+}
+
+
+for ( let i = 1; i <= 5; i++) {
+  setTimeout(function timer(){
+    console.log(i);
+  }, i * 1000)
+}
+
+for ( var i = 1; i <= 5; i++) {
+  (function (j) {
+    setTimeout(function timer(){
+      console.log(j);
+    }, j * 1000)
+  })(i)
+}
+
+for ( var i = 1; i <= 5; i++) {
+  let j = i;
+  setTimeout(function timer(){
+    console.log(j);
+  }, j * 1000)
+}
