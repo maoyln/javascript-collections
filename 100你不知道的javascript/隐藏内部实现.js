9de732bad3875759ddda18a5fba62ec0a641ad3a
maoyln <maoyl_yx@163.com>
2025-04-28 10:55:24 +0800

    隐藏

--- 100你不知道的javascript/隐藏内部实现.js
+++ 100你不知道的javascript/隐藏内部实现.js
@@ -1,12 +1,24 @@
+// function doSomething(a) {
+//     console.log("Doing something...");
+//     b = a + doSomethingElse(a * 2)
+//     console.log(b * 3);  // 15
+// }
+
+// function doSomethingElse(a) {
+//     console.log("Doing something else...");
+//     return a - 1;
+// }
+// var b;
+
 function doSomething(a) {
     console.log("Doing something...");
     b = a + doSomethingElse(a * 2)
     console.log(b * 3);  // 15
+    var b;
+    function doSomethingElse(a) {
+        console.log("Doing something else...");
+        return a - 1;
+    }
 }
 
-function doSomethingElse(a) {
-    console.log("Doing something else...");
-    return a - 1;
-}
-var b;
 doSomething(2);
