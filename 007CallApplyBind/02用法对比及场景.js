2162876ecdf73929b74925aed48d43e670922a85
maoyln <maoyl_yx@163.com>
2025-04-29 11:22:06 +0800

    108闭包的理解-现代模块化机制

--- 007CallApplyBind/02用法对比及场景.js
+++ 007CallApplyBind/02用法对比及场景.js
@@ -16,6 +16,28 @@ function Food(name, price) {
   this.category = 'food';
 }
 
+// 注意上述代码，可以用下面代码替代（方便理解）--该替代方案估计会有问题，用下面一个方案比较好【const instance = Object.create(Food.prototype);】
+// function Food(name, price) {
+//   Product.call(Food.prototype, name, price); // 使用 call 方法继承 Product 构造函数
+//   this.category = 'food';
+// }
+
+// 注意上述代码，可以用下面代码替代（方便理解）
+// function Food(name, price) {
+//   const instance = Object.create(Food.prototype);
+//   Product.call(instance, name, price);
+//   instance.category = 'food';
+//   return instance;
+// }
+
+// 注意上述代码，可以用下面代码替代（方便理解）
+// function Food(name, price) {
+//   const obj = {};
+//   Product.call(obj, name, price); // 使用新对象替代this
+//   obj.category = 'food';
+//   return obj; // 必须手动返回
+// }
+
 const apple = new Food('Apple', 2);
 console.log(apple.name);  // 输出: "Apple"
 console.log(apple.category);  // 输出: "food"
@@ -23,3 +45,21 @@ console.log(apple);  // 输出: Food { name: 'Apple', price: 2, category: 'food'
 
 console.log('--------------------------------------华丽分割线--------------------------------------');
 
+
+
+
+// ES6替代方案
+
+// class Product {
+//   constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
+// }
+
+// class Food extends Product {
+//   constructor(name, price) {
+//     super(name, price); // 替代了Product.call(this,...)
+//     this.category = 'food';
+//   }
+// }
