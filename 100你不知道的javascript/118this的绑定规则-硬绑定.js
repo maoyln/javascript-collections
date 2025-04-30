7185b02a6f49a008f8b573767afcd2077fd2b33c
maoyln <maoyl_yx@163.com>
2025-04-30 14:51:41 +0800

    110-this的深入理解-this硬绑定

--- 100你不知道的javascript/118this的绑定规则-硬绑定.js
+++ 100你不知道的javascript/118this的绑定规则-硬绑定.js
@@ -19,3 +19,63 @@ bar();  // 2
 setTimeout(bar, 200);  // 2 
 
 bar.call(global || window); // 2 
+
+console.log('---------------------------------------华丽分割线--------------------------------');
+
+// 使用方法是创建一个可以重复使用的辅助函数
+function foo1(something) {
+  console.log(this.a, something);
+  return this.a + something;
+}
+
+var obj1 = {
+  a: 2
+}
+var bar1 = function() {
+  return foo1.apply(obj1, arguments);
+}
+
+var b = bar1(3); // 2, 3
+console.log(b); // 5
+// 友情提示：此处打印结果阅读要注意上面的setTimeout
+
+console.log('---------------------------------------华丽分割线--------------------------------');
+
+// 领一种使用方法是创建一个可以重复使用的辅助函数
+
+function foo2(something) {
+  console.log(this.a, something);
+  return this.a + something;
+}
+
+function bind(fn, obj) {
+  return function() {
+    return fn.apply(obj, arguments)
+  }
+}
+
+var obj2 = {
+  a: 2
+}
+var bar2 = bind(foo2, obj2)
+
+var b2 = bar2(3); // 2, 3
+console.log(b2); // 5
+
+console.log('---------------------------------------华丽分割线--------------------------------');
+
+// 由于硬绑定是一种非常常用的模式，所以ES5提供了内置的方法Function.prototype.bind
+
+function foo3(something) {
+  console.log(this.a, something);
+  return this.a + something;
+}
+
+
+var obj3 = {
+  a: 2
+}
+var bar3 = foo3.bind(obj3)
+
+var b3 = bar3(3); // 2, 3
+console.log(b3); // 5
