5d53d87684f230eabb1e4f1a4392a2efb13636ca
maoyln <maoyl_yx@163.com>
2025-04-30 15:59:27 +0800

    this绑定-箭头函数

---
+++ 100你不知道的javascript/123this词法-箭头函数.js
@@ -0,0 +1,18 @@
+function foo() {
+  return (a) => {
+    console.log(this.a);
+  }
+}
+
+var obj1 = {
+  a: 2
+}
+
+var obj2 = {
+  a: 3
+}
+
+var bar = foo.call(obj1);
+bar.call(obj2); // 2
+
+// foo()内部创建的箭头函数会捕获调用时foo()的this。由于foo()的this绑定到obj1, bar（引用箭头函数）的this也会绑定到obj1，箭头函数的绑定无法被修改。
