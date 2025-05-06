dc7b75aa3686471df1496dd6a3254c93df0f54ab
maoyln <maoyl_yx@163.com>
2025-05-06 17:51:09 +0800

    数组的操作

---
+++ 200你不知道的javascript中卷/203声明相关.js
@@ -0,0 +1,11 @@
+var a;
+
+console.log(typeof a);
+
+
+var b = 32;
+var c
+b = c
+
+console.log(typeof b);
+console.log(typeof c);
---
+++ 200你不知道的javascript中卷/204数组删除.js
@@ -0,0 +1,20 @@
+var a = [1,2,3,4]
+
+console.log(a); // [ 1, 2, 3, 4 ]
+console.log(a.length); // 4
+
+delete a[2]
+
+console.log(a); // [ 1, 2, <1 empty item>, 4 ]
+console.log(a.length); // 4
+
+console.log('---------------------------------------华丽分割线--------------------------------');
+
+var b = [];
+
+b[0] = 1;
+b['test'] = 2
+
+console.log(b.length);
+console.log(b['test']);
+console.log(b.test);
