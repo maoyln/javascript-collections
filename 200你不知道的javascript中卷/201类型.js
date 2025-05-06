b0e4c721d245e59aecb035112ea4498874480202
maoyln <maoyl_yx@163.com>
2025-05-06 17:33:30 +0800

    类型判断

---
+++ 200你不知道的javascript中卷/201类型.js
@@ -0,0 +1,33 @@
+/*************  ✨ Windsurf Command ⭐  *************/
+// 类型判断
+function isType(type) {
+  return function (obj) {
+    return Object.prototype.toString.call(obj) === `[object ${type}]`;
+  };
+}
+
+// 使用
+const isString = isType('String');
+const isNumber = isType('Number');
+const isBoolean = isType('Boolean');
+const isArray = isType('Array');
+const isFunction = isType('Function');
+const isNull = isType('Null');
+const isUndefined = isType('Undefined');
+const isRegExp = isType('RegExp');
+const isDate = isType('Date');
+const isError = isType('Error');
+const isSymbol = isType('Symbol');
+
+console.log(isString('hello')); // true
+console.log(isNumber(123)); // true
+console.log(isBoolean(true)); // true
+console.log(isArray([1, 2, 3])); // true
+console.log(isFunction(function () {})); // true
+console.log(isNull(null)); // true
+console.log(isUndefined(undefined)); // true
+console.log(isRegExp(/abc/)); // true
+console.log(isDate(new Date())); // true
+console.log(isError(new Error())); // true
+console.log(isSymbol(Symbol())); // true
+/*******  56f4d7f1-9371-4ede-8474-10cb9075f135  *******/
