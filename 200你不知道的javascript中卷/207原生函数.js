2d38eb6afed0fc772b9d71a22b29f3900287a3e5
maoyln <maoyl_yx@163.com>
2025-05-22 14:37:38 +0800

    原生函数

--- 200你不知道的javascript中卷/207原生函数.js
+++ 200你不知道的javascript中卷/207原生函数.js
@@ -52,16 +52,17 @@ console.log(fn instanceof Number); // true
 
 console.log('---------------------------------------华丽分割线--Boolean--------------------------------');
 // Boolean()
-var a1 = new Boolean( false );
+var a1 = new Boolean(false);
 console.log(a1); // [Boolean: false]
 if (!a1) {
-  console.log( "Oops" ); // 执行不到这里
+  console.log("Oops"); // 执行不到这里
 }
 
 console.log('---------------------------------------华丽分割线--Array--------------------------------');
 // Array()
 
 // 构造函数 Array(..) 不要求必须带 new 关键字。不带时，它会被自动补上。 因此 Array(1,2,3) 和 new Array(1,2,3) 的效果是一样的。
+// Array 构造函数只带一个数字参数的时候，该参数会被作为数组的预设长度(length)，而 非只充当数组中的一个元素。
 var aar = Array(1, 2, 3);
 console.log(aar); // [1, 2, 3]
 console.log(aar instanceof Array); // true
@@ -78,33 +79,59 @@ var dar = Array(3);
 console.log(dar); // [undefined, undefined, undefined]
 console.log(dar instanceof Array); // true
 
-var ear = Array.apply( null, { length: 3 } );
+var ear = Array.apply(null, {
+  length: 3
+});
 console.log(ear); // [undefined, undefined, undefined]
 console.log(ear instanceof Array); // true
 
 
 console.log('---------------------------------------华丽分割线--Object--------------------------------');
-
+// 同样，除非万不得已，否则尽量不要使用 Object(..)/Function(..)/RegExp(..):
 Object()
 
 console.log('---------------------------------------华丽分割线--Function--------------------------------');
-
+// 同样，除非万不得已，否则尽量不要使用 Object(..)/Function(..)/RegExp(..):
 Function()
 
 
 console.log('---------------------------------------华丽分割线--RegExp--------------------------------');
+// 同样，除非万不得已，否则尽量不要使用 Object(..)/Function(..)/RegExp(..):
 RegExp()
 
 console.log('---------------------------------------华丽分割线--Date--------------------------------');
 Date()
 
 console.log('---------------------------------------华丽分割线--Error--------------------------------');
-Error()
+// Error()
+
+var aer = new Error();
+console.log(aer);
+console.log(aer instanceof Error);
+
+var ber = new Error('test');
+console.log(ber);
+console.log(ber instanceof Error);
 
 console.log('---------------------------------------华丽分割线--Symbol--------------------------------');
 Symbol() // ES6 中新加入的!
 
+// 我们可以使用 Symbol(..) 原生构造函数来自定义符号。但它比较特殊，不能带 new 关键字，否则会出错:
+var asy = Symbol('a');
+var bsy = Symbol('b');
+var csy = Symbol('c');
+var dsy = Symbol('d');
+var esy = Symbol('e');
+var fsy = Symbol('f');
+var gsy = Symbol('g');
 
+console.log(asy); // Symbol(a)
+console.log(bsy); // Symbol(b)
+console.log(csy); // Symbol(c)
+console.log(dsy); // Symbol(d)
+console.log(esy); // Symbol(e)
+console.log(fsy); // Symbol(f)
+console.log(gsy); // Symbol(g)
 console.log('---------------------------------------华丽分割线--拆封--------------------------------');
 // 拆封
 // 如果想要得到封装对象中的基本类型值，可以使用 valueOf() 函数:
@@ -116,4 +143,3 @@ console.log(e.valueOf());
 console.log(f.valueOf());
 console.log(g.valueOf());
 console.log(a1.valueOf());
-
