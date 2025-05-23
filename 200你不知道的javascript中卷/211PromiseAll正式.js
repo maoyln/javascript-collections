8f96d4effde89bd0db62f646510afbf4ddf3b56d
maoyln <maoyl_yx@163.com>
2025-05-23 14:11:41 +0800

    promise

--- 200你不知道的javascript中卷/211PromiseAll正式.js
+++ 200你不知道的javascript中卷/211PromiseAll正式.js
@@ -19,9 +19,13 @@ function add(aPromise, bPromise, fn) {
 
 // 使用示例
 add(
-  Promise.resolve(function(fna) { return fna(8); }),
-  Promise.resolve(function(fna) { return fna(2); }),
-  function(a, b) {
+  Promise.resolve(function (fna) {
+    return fna(8);
+  }),
+  Promise.resolve(function (fna) {
+    return fna(2);
+  }),
+  function (a, b) {
     console.log(a + b); // 输出 10
   }
 );
@@ -39,7 +43,33 @@ function add1(aPromise, bPromise, fn) {
 add1(
   Promise.resolve(8),
   Promise.resolve(2),
-  function(a, b) {
+  function (a, b) {
     console.log(a + b); // 输出 10
   }
 );
+
+
+console.log('---------------------------------------华丽分割线--------------------------------');
+
+// 超时
+function timeoutPromise(delay) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      // resolve("Timeout!"); // 完成  *****重要重要******
+      reject("Timeout!"); // 失败
+    }, delay);
+  });
+}
+// 设置foo()超时 
+Promise.race([
+  timeoutPromise(4000),
+  timeoutPromise(3000)
+]).then(
+  function () {
+    // foo(..)及时完成!
+    console.log('foo()及时完成!');
+  },
+  function (err) {
+    console.log(err, 'foo()被拒绝，或者只是没能按时完成');
+    // 或者foo()被拒绝，或者只是没能按时完成 // 查看err来了解是哪种情况
+  });
